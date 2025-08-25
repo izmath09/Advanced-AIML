afd9bd8ad5cdfc1821ba596e75a18c79caff5f06
izmath09 <izmafathima24@gmail.com>
2025-08-25 20:00:35 +0530

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,4 @@
+from datetime import datetime
+
+now = datetime.now()
+print("Current time:", now.strftime("%Y-%m-%d %H:%M:%S"))
---
+++ 4.py
@@ -0,0 +1,4 @@
+import random
+
+toss = random.choice(["Heads", "Tails"])
+print("Coin Toss:", toss)
